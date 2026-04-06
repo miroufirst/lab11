1e04479494527d63e8d6b8837fafa3b14b8f1ae5
Yevhenii Hubych <yevhen199606@gmail.com>
2026-04-06 13:48:06 +0300

    Add project files.

---
+++ WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbWagonType = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // cbWagonType
+            // 
+            this.cbWagonType.FormattingEnabled = true;
+            this.cbWagonType.Items.AddRange(new object[] {
+            "Locomotive",
+            "Passenger",
+            "Cargo"});
+            this.cbWagonType.Location = new System.Drawing.Point(61, 29);
+            this.cbWagonType.Name = "cbWagonType";
+            this.cbWagonType.Size = new System.Drawing.Size(121, 24);
+            this.cbWagonType.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(215, 30);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Додати вагон";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.Location = new System.Drawing.Point(394, 29);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(823, 458);
+            this.pnlCanvas.TabIndex = 2;
+            this.pnlCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCanvas_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1251, 578);
+            this.Controls.Add(this.pnlCanvas);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbWagonType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbWagonType;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel pnlCanvas;
+    }
+}
+
---
+++ WindowsFormsApp18/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public abstract class WagonHandler
+    {
+        protected WagonHandler Successor;
+        public void SetSuccessor(WagonHandler successor) => this.Successor = successor;
+        public abstract void HandleDraw(string type, Graphics g, int x, int y);
+    }
+
+    public class LocomotiveHandler : WagonHandler
+    {
+        public override void HandleDraw(string type, Graphics g, int x, int y)
+        {
+            if (type == "Locomotive")
+            {
+                g.FillRectangle(Brushes.Firebrick, x, y, 60, 40);
+                g.FillRectangle(Brushes.Black, x + 45, y - 10, 10, 15);
+                g.FillEllipse(Brushes.Black, x + 5, y + 35, 15, 15);
+                g.FillEllipse(Brushes.Black, x + 40, y + 35, 15, 15);
+            }
+            else Successor?.HandleDraw(type, g, x, y);
+        }
+    }
+
+    public class PassengerHandler : WagonHandler
+    {
+        public override void HandleDraw(string type, Graphics g, int x, int y)
+        {
+            if (type == "Passenger")
+            {
+                g.FillRectangle(Brushes.RoyalBlue, x, y, 60, 40);
+                g.FillRectangle(Brushes.LightSkyBlue, x + 8, y + 8, 18, 15);
+                g.FillRectangle(Brushes.LightSkyBlue, x + 34, y + 8, 18, 15);
+                g.FillEllipse(Brushes.Black, x + 5, y + 35, 15, 15);
+                g.FillEllipse(Brushes.Black, x + 40, y + 35, 15, 15);
+            }
+            else Successor?.HandleDraw(type, g, x, y);
+        }
+    }
+
+    public class CargoHandler : WagonHandler
+    {
+        public override void HandleDraw(string type, Graphics g, int x, int y)
+        {
+            if (type == "Cargo")
+            {
+                g.FillRectangle(Brushes.SaddleBrown, x, y + 5, 60, 35);
+                g.DrawLine(Pens.White, x, y + 5, x + 60, y + 40);
+                g.FillEllipse(Brushes.Black, x + 5, y + 35, 15, 15);
+                g.FillEllipse(Brushes.Black, x + 40, y + 35, 15, 15);
+            }
+            else Successor?.HandleDraw(type, g, x, y);
+        }
+    }
+
+
+    public partial class Form1 : Form
+    {
+        private List<string> train = new List<string>();
+        private WagonHandler chain;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupChain();
+        }
+        private void SetupChain()
+        {
+            var loco = new LocomotiveHandler();
+            var pass = new PassengerHandler();
+            var cargo = new CargoHandler();
+
+            loco.SetSuccessor(pass);
+            pass.SetSuccessor(cargo);
+
+            chain = loco;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cbWagonType.SelectedItem != null)
+            {
+                train.Add(cbWagonType.SelectedItem.ToString());
+                pnlCanvas.Invalidate();
+            }
+        }
+
+        private void pnlCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            int startX = 20;
+            int startY = 80;
+            int wagonWidth = 60;
+            int gap = 15;
+
+            for (int i = 0; i < train.Count; i++)
+            {
+                int currentX = startX + (i * (wagonWidth + gap));
+
+                chain.HandleDraw(train[i], e.Graphics, currentX, startY);
+
+                if (i < train.Count - 1)
+                {
+                    e.Graphics.DrawLine(Pens.Black, currentX + wagonWidth, startY + 30, currentX + wagonWidth + gap, startY + 30);
+                }
+            }
+        }
+    }
+}
+  
